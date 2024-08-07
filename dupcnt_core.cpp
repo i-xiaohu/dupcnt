7f5fe25ae42ce4199f7d59badbed7b19eeaa8dc8
i-xiaohu <631751599@qq.com>
2024-08-08 00:46:13 +0800

    dupcnt_core.cpp: bug fixed and program validated by sorting.

--- dupcnt_core.cpp
+++ dupcnt_core.cpp
@@ -7,6 +7,8 @@
 #include <cstring>
 #include <string>
 #include <cassert>
+#include <sys/time.h>
+#include <algorithm>
 #include "dupcnt_core.h"
 
 #define MAX_LINE 10240
@@ -48,7 +50,7 @@ void Trie::add_read(int n, const char *s) {
 			case 'C': c = 1; break;
 			case 'G': c = 2; break;
 			case 'T': c = 3; break;
-			default: c = 4; break;
+			default: c = 0; break;
 		}
 		if (nodes[p].x[c] == 0) { // No child
 			nodes[p].x[c] = nodes.size();
@@ -80,18 +82,46 @@ void Trie::stat() {
 				}
 			}
 		}
-		fprintf(stderr, "layer %d, nodes %d\n", i + 1, curr_n);
 		std::swap(prev_n, curr_n);
 		std::swap(prev_a, curr_a);
 	}
+	unique_n = prev_n; // Leaves are those unique reads
+	int max_occ = 0, max_id = -1;
 	for (int i = 0; i < prev_n; i++) {
 		int leaf = prev_a[i];
 		assert(nodes[leaf].x[0] > 0);
-		if (nodes[leaf].x[0] == 1) {
-			unique_n++;
+		if (nodes[leaf].x[0] > max_occ) {
+			max_occ = nodes[leaf].x[0];
+			max_id = leaf;
 		}
 	}
+	// Linear scan to restore the most repetitive read
+	std::string rep_read;
+	int child = max_id;
+	for (int i = max_id - 1; i >= 0; i--) {
+		for (int j = 0; j < 4; j++) {
+			if (nodes[i].x[j] == child) {
+				rep_read += "ACGT"[j];
+				child = i;
+				break;
+			}
+		}
+	}
+	assert(rep_read.length() == read_length_monitor);
+	std::reverse(rep_read.begin(), rep_read.end());
+	fprintf(stderr, "The most repetitive read occurs %d times:\n", max_occ);
+	fprintf(stderr, "%s\n", rep_read.c_str());
+	free(prev_a);
+	free(curr_a);
 	fprintf(stderr, "%d unique reads in %d reads\n", unique_n, reads_n);
+}
+
+double realtime()
+{
+	struct timeval tp;
+	struct timezone tzp;
+	gettimeofday(&tp, &tzp);
+	return tp.tv_sec + tp.tv_usec * 1e-6;
 }
 
 void process(int n_threads, int n_sample, char *files[]) {
@@ -103,18 +133,56 @@ void process(int n_threads, int n_sample, char *files[]) {
 			fprintf(stderr, "Open `%s` failed\n", files[i]);
 			continue;
 		}
-		int n_seqs = 0;
+		std::vector<std::string> all_seqs;
 		std::vector<std::string> seqs;
 		Trie counter;
 		while (true) {
 			seqs = input_reads(fp, actual_batch_size);
 			if (seqs.empty()) break;
-			n_seqs += seqs.size();
-			for (const auto &r : seqs) {
-				counter.add_read(r.length(), r.data());
+			for (auto &r : seqs) {
+				for (auto &c : r) {
+					if (c == 'N') c = 'A';
+				}
+				counter.add_read(r.length(), r.c_str());
+				all_seqs.push_back(r);
 			}
 		}
-		fprintf(stderr, "%d seqs in sample %d\n", n_seqs, i + 1);
+		double t_start, t_end;
+		t_start = realtime();
 		counter.stat();
+		t_end = realtime();
+		fprintf(stderr, "Visiting trie in %.2f seconds\n", t_end - t_start);
+
+		// Validating using brute-force
+		t_start = realtime();
+		std::sort(all_seqs.begin(), all_seqs.end());
+		int unique = 1, last = 0, occ = 0, max_occ = 0;
+		for (int j = 1; j < all_seqs.size(); j++) {
+			if (all_seqs[j] != all_seqs[j-1]) {
+				unique++;
+				occ = j - last;
+				last = j;
+				max_occ = std::max(max_occ, occ);
+			}
+		}
+		fprintf(stderr, "Sorting: the most repetitive read appears %d times\n", max_occ);
+		last = 0;
+		for (int j = 1; j < all_seqs.size(); j++) {
+			if (all_seqs[j] != all_seqs[j-1]) {
+				occ = j - last;
+				last = j;
+				if (occ == max_occ) {
+					fprintf(stderr, "%s\n", all_seqs[j-1].c_str());
+					break;
+				}
+			}
+		}
+		t_end = realtime();
+		fprintf(stderr, "Sort reads in %.2f seconds\n", t_end - t_start);
+
+		assert(all_seqs.size() == counter.reads_n);
+		if (unique != counter.unique_n) {
+			fprintf(stderr, "Sort found %d unique reads\n", unique);
+		}
 	}
 }
